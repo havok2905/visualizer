d2e29b1a067065ecc27640f4157b8879c6cd3f21
Christopher McLean <havok2905@gmail.com>
2015-12-30 00:18:52 -0500

    working line graph

--- app/scripts/components/visualizer.js
+++ app/scripts/components/visualizer.js
@@ -1,10 +1,12 @@
 'use strict';
 
 import React from 'react';
+import AUDIO_CONTEXT from '../constants/audio_context';
+import GRAPH_CONFIG from  '../constants/graph_config';
 import AudioStream from '../services/audio_stream';
-import AudioContext from '../constants/audio_context';
 import VisualizerActions from './visualizer_actions';
 import Graph from './graph';
+import LineGraph from './line_graph';
 
 class Visualizer extends React.Component {
   constructor(props) {
@@ -15,7 +17,7 @@ class Visualizer extends React.Component {
   componentWillMount() {
     this.stream = new AudioStream({
       audio:     new Audio('/app/assets/master_of_puppets.mp3'),
-      context:   new AudioContext(),
+      context:   new AUDIO_CONTEXT(),
       frequency: new Uint8Array(200)
     });
 
@@ -30,6 +32,13 @@ class Visualizer extends React.Component {
     return (
       <div className='visualizer'>
         <Graph data={ this.state.data }/>
+        <LineGraph line={ GRAPH_CONFIG.line }
+                   xscale={ GRAPH_CONFIG.xscale }
+                   yscale={ GRAPH_CONFIG.yscale }
+                   width={ GRAPH_CONFIG.width }
+                   height={ GRAPH_CONFIG.height }
+                   data={ this.state.data }
+        />
         <VisualizerActions song={ this.props.song }
                           stream={ this.stream }/>
       </div>
